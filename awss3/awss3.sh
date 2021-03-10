62b085239515ef5a459d3d86a9a394a675ebc03c
Arshad Kazmi <arshadkazmi42@gmail.com>
2021-03-10 14:36:07 +0530

    More support added for awss3

--- awss3/awss3.sh
+++ awss3/awss3.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
+echo "\n\n========= $1 ============"
+
 echo "Testing bucket: "$1
 
+echo "List contents"
 aws s3 ls s3://$1
+
+echo "List with flag"
+aws s3 ls s3://$1 --no-sign-request
 
 echo "Trying to upload"
 
 aws s3 cp takeover.html s3://$1/takeover.html
+aws s3 cp takeover.html s3://$1/takeover.html --no-sign-request
 
 aws s3 mv takeover.html s3://$1/takeover.html
+aws s3 mv takeover.html s3://$1/takeover.html --no-sign-request
+
+echo "=============== END ==========\n\n"
